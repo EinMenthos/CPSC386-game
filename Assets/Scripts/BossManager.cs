5ef535c43f77b5cdfd3e4d912f980b47f95c3d8a
Daniel Chen Wu <59303591+EinMenthos@users.noreply.github.com>
2024-04-21 23:43:33 -0700

    boss changes according to hp

--- Assets/Scripts/BossManager.cs
+++ Assets/Scripts/BossManager.cs
@@ -8,12 +8,16 @@ public class BossManager : MonoBehaviour
     [SerializeField] public int BossHP = 10;
     //public float spawnsPerSecond = 0.5f;
     //[SerializeField] public bool usePooling = false;
-    //[SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject BossGO;
     //public UnitPool pool {get; protected set;}
     //float timer = 0;
     int curHP = 0;//Should increment up to maxEnemiesSpawned
     EnemyScoreController1 EnScCtr1;
     //EnemyScoreController2 EnScCtr2;
+
+    public SpriteRenderer spriteRenderer;
+    public Sprite b2;
+    public Sprite b3;
 /*
     //fade in effect while destroying/disabling it
     public float fadeDelay = 0.3f;
@@ -40,6 +44,17 @@ public class BossManager : MonoBehaviour
     public void HandleEnemy(GameObject other)//Deletes enemy and applies damage
     {
         curHP++;
+        if(curHP > BossHP * 2/3){
+            Debug.Log("Boss 2/3!");
+            spriteRenderer.sprite = b3;
+        }
+        else if(curHP > BossHP * 1/3){
+            Debug.Log("Boss 1/3!");
+            
+            spriteRenderer.sprite = b2;
+        }
+        
+
         if (curHP < BossHP){
             Debug.Log(curHP + " / " + BossHP);
         }
